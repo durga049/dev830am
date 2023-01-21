4d01c572c2edab46636f98b8d256106ac6c790f0
sathyadevops <sathyadevops1@gmail.com>
2023-01-21 03:54:57 +0000

    arith ops

---
+++ operations.c
@@ -0,0 +1,8 @@
+void main()
+{
+  printf(" Arithmetic Operations...");
+  printf(" Arithmetic Operations...");
+  printf(" Arithmetic Operations...");
+  printf(" Arithmetic Operations...");
+  printf(" Arithmetic Operations...");
+}	
